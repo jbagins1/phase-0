eac47b0c314d7eaa84f305a2e5942a109500cbff
Jack Baginski <jbagins1@gmail.com>
2016-01-26 21:31:49 -0600

    Add 4.3 files

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,37 @@
+puts "What is your first name?"
+first_name = gets.chomp.capitalize
+puts "What is your middle name?"
+middle_name = gets.chomp.capitalize
+puts "What is your last name?"
+last_name = gets.chomp.capitalize
+puts "Greetings #{first_name} #{middle_name} #{last_name}"
+
+puts "What is your favorite number?"
+number = gets.chomp.to_i + 1
+better_number = number
+puts "How about #{better_number}, its bigger and better number!"
+
+# How do you define a local variable?
+
+A local variable is local in scope and can only be accessed in certain places. For example, if a variable is defined in a method, that variable defined outside of the method can have a different value assigned to it although both variables can be named the same.
+
+How do you define a method?
+
+Methods are blocks of code the computer executes instead of having to write code over and over.
+
+What is the difference between a local variable and a method?
+
+Variables store information assigned to them. Meanwhile methods allow us to execute a piece of code repeadtedly.
+
+How do you run a ruby program from the command line?
+
+Enter the terminal, not irb. Type in "ruby [insert file name here].rb"
+
+How do you run an RSpec file from the command line?
+
+Enter the terminal, not irb. Type in "rspec [insert file name here].rb"
+
+What was confusing about this material? What made sense?
+
+This was pretty straight forward and a review of some of the first few Code Academy lesson. I did, however, learn how to run the rb file from the terminal and how to rspec.
+
