c322b057e6bd78552a8ef21db2939b7cfb3312cd
Jack Baginski <jbagins1@gmail.com>
2016-01-27 17:16:18 -0600

    Add 4.5.5 files

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,15 @@
+# Simple Substrings
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def welcome(x)
+  y = x.upcase
+  if y =~ /CA/
+    puts "Welcome to California"
+  else
+    puts "You should move to California"
+  end
+end
