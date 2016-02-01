4c26805e2a67efa965e0a848360f468a7f347aaf
Jack Baginski <jbagins1@gmail.com>
2016-01-31 20:31:47 -0600

    Update 5.2 files

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -2,14 +2,11 @@
 
 # I worked on this challenge by with: Brett Ripley.
 
-# I spent [] hours on this challenge.
-
+# I spent 1 hours on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.
-
-
 
 # 0. Pseudocode
 
@@ -32,22 +29,35 @@ def pad!(array, min_size, value = nil)
 end
 
 def pad(array, min_size, value = nil)
-  array1 = []
+  new_array = []
   array.each do |x|
-    array1.push(x)
+    new_array.push(x)
   end
   if array.length >= min_size
-    array1
+    new_array
   else
-    while array1.length < min_size
-      array1.push(value)
+    while new_array.length < min_size
+      new_array.push(value)
     end
-  return array1
+  return new_array
   end
 end
 
 # 3. Refactored Solution
 
-
+I couldn't figure out how to refactor this any further.
 
 # 4. Reflection
+
+# Were you successful in breaking the problem down into small steps?
+# Yes.
+# Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
+# Somewhat. Its tough because I feel like I know what I want the code to do, but its still difficult to find the proper syntax to employ.
+# Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
+# Yes.
+# When you refactored, did you find any existing methods in Ruby to clean up your code?
+# No.
+# How readable is your solution? Did you and your pair choose descriptive variable names?
+# I think its fairly readable. Yes we chose descriptive variable names.
+What is the difference between destructive and non-destructive methods in your own words?
+# Non-destructive methods don't modify the original data structure. Destructive methods modify the original data structure.
