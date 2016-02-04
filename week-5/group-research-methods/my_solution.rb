a443b11e6ab110d1f709821f9e188cfd6cf1526d
Jack Baginski <jbagins1@gmail.com>
2016-02-04 13:37:53 -0600

    Update 5.4.5 files

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -52,17 +52,25 @@ end
 
 # Person 4
 def my_array_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.delete_if { |item| item.to_s.include? thing_to_delete}
 end
 
 def my_hash_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.delete_if { |k, v| k.to_s.include? thing_to_delete}
 end
 
-# Identify and describe the Ruby method(s) you implemented.
+# Release 1: Identify and describe the Ruby method you implemented. Teach your
+# accountability group how to use the methods.
+#=>The method I used was the delete_if method. It will delete an element
+#=>from the array/hash if some condition is met. In this case I deleted if
+#=>an element had a substring of thing_to_delete.
+# Release 3: Reflect!
+# What did you learn about researching and explaining your research to others?
+#=>My biggest difficulty was doing destructive methods. I could get the correct
+#=>answer, but it would not be the same object_id which forced me to abandon
+#=>my initial solution.
 #
-#
-#
+
 
 
 # Person 5
